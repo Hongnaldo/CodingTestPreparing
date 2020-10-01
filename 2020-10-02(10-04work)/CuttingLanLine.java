e21525165b152c2aa8dfc14af025dced1a03ffb5
ronaldo842 <ronaldo842@naver.com>
2020-10-02 00:54:30 +0900

    new file

---
+++ 2020-10-02(10-04work)/CuttingLanLine.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+public class CuttingLanLine {
+	static long K,N;
+	static long line[];
+	public static long pro(long key)
+	{
+		long left =1;
+		long right = key;
+		long mid = 0;
+		boolean flag = true;
+		while(right>=left)
+		{
+			mid = (left+right)/2;
+			int sum =0;
+			for(int i=0; i<K; i++)
+			{
+				sum += line[i]/mid;
+			}
+			
+			if(sum>=N)
+			{
+				left = mid+1;	
+			}
+			else if(sum<N)
+			{
+				right = mid-1;
+				
+			}
+			
+			
+		}
+		
+		return right;
+		
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str);
+		
+		K = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		
+		line = new long[(int) K];
+		for(int i=0; i<K; i++)
+		{
+			line[i] = Long.parseLong(br.readLine());
+		}
+		
+		Arrays.sort(line);
+		long key = line[(int) (K-1)];
+		
+		
+		long x =pro(key);
+		System.out.println(x);
+	}
+
+}
---
+++ 2020-10-02(10-04work)/SooFind.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+public class SooFind {
+	static int N;
+	static int[] nA;
+	static int M;
+	static int[] mA;
+	public static void pro()
+	{
+		boolean flag = true;
+		
+		for(int i=0; i<M; i++)
+		{
+			int min = 0;
+			int max = nA.length-1;
+			int mid = (min + max)/2;
+			while(flag)
+			{
+				if(mA[i] == nA[mid])
+				{
+					System.out.println(1);
+					break;
+				}
+				if(mid>=max)
+				{
+					System.out.println(0);
+					break;
+				}
+				if(mA[i]>nA[mid])
+				{
+					min = mid;
+					if((mid+max)%2 ==1)
+						mid = (mid+max)/2+1;
+					mid = (mid+max)/2;
+				}
+				else if(mA[i]<nA[mid])
+				{
+					max= mid;
+					mid = (min+mid)/2;
+				}
+				
+					
+			}
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		nA = new int[N];
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str);
+		for(int i=0; i<N;i++)
+			nA[i] = Integer.parseInt(st.nextToken());
+		
+		M = Integer.parseInt(br.readLine());
+		mA = new int[M];
+		str = br.readLine();
+		st = new StringTokenizer(str);
+		for(int i=0; i<M; i++)
+			mA[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(nA);
+	
+		
+		pro();
+		
+		
+
+	}
+
+}
